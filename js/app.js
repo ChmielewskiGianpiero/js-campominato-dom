1494b819c9b69707cb0c83845f58851906ce27ef
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-17 00:11:11 +0200

    ADD - bombs and btn-reset

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@ const gridDOMElement = document.querySelector(".grid");
 
 const btnPlayDOMElement = document.getElementById("btn-play");
 
+const btnResetDOMElement = document.getElementById("btn-reset")
+
+
 btnPlayDOMElement.addEventListener("click", function () {
     resetGrid();
     createGrid(100);
     selectedCell();
-    bombsGenerator(10,20)
 });
+
+btnResetDOMElement.addEventListener("click", function(){
+    resetGrid();
+})
+
+
+let bombs = bombsGenerator(1,100,16)
+console.log (bombs)
 
 
 function resetGrid (){
@@ -16,7 +26,7 @@ function resetGrid (){
 
 function createGrid (num){
     for (let i = 0; i < num; i++){
-        n = i + 1;
+        const n = i + 1;
         let cellHtml = `<div class="cell">${n}</div>`;
         gridDOMElement.innerHTML = gridDOMElement.innerHTML + cellHtml;
     };
@@ -26,22 +36,40 @@ function selectedCell(){
     const cellDOMElements = document.querySelectorAll(".cell");
     for (let i = 0; i < cellDOMElements.length; i++){
         const currentCell = cellDOMElements[i];
-        
         currentCell.addEventListener("click", function(){
-            console.log("Hai scelto il numero: " + (i + 1));
-            currentCell.classList.add("bg-selected");
+        const n = i + 1
+        console.log(n, bombs);
+        if (bombs.includes(n)){
+            currentCell.classList.add("bg-red")
+            alert("hai perso, per ricominciare premi su reset");
+        }else{
+            currentCell.classList.add("bg-blue");
+        }
         })
     };
 }
 
-
-
 function bombsGenerator(rangeMin, rangeMax, number){
-    getRandomIntInclusive(rangeMin, rangeMax)
+    const bombsArray = []
+
+    while (bombsArray.length < number ){
+        const n = getRandomIntInclusive(rangeMin, rangeMax)
+        if (bombsArray.includes(n) === false){
+            bombsArray.push(n)
+        }
+    }
+    return bombsArray
 }
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); 
-  }
+}
+
+ function gameOver(){
+    alert("hai perso"); 
+}
+
+
+
